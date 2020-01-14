31b683151d804e0f80b794cd376c258012702250
Dmitriy Padalka <padalka.dmitriy@gmail.com>
2020-01-15 00:55:42 +0200

    Stage changes. Renamed some functions Added comments to some functions Added TODO Redirection of beta const to subfunction

--- Main.py
+++ Main.py
@@ -1,56 +1,89 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
+from itertools import repeat
 from pickle import load
 from random import random
 from time import time
-from copy import deepcopy
 
 
 # region functions
 def get_raw_value_of_path(distance, pheromone, beta):
-    '''Calculates value of the specific path based on it's distance,
+    """Calculates value of the specific path based on it's distance,
     pheromone, and beta values
+
+    Return 0 to avoid zero division error and due to further usage of
+    that function where. -1 is the representative of way that should not be
+    attended, and 0 as a return of this function help to build proper execution
+    of get_random_way_from_list function(0 is the way to ignore attended ways)
 
     :param distance: The distance value of the path
     :type distance: float
     :param pheromone: Value of the pheromone on that path
     :type pheromone: float
-    :param beta: Coefficient of how mych ants relies on pheromone value
+    :param beta: Coefficient of how much ants relies on pheromone value, gets passed to subfunction
     :type beta: float
     :return: Returns value calculated by formula if distance is bigger
-    than zero, to avoid ZeroDivisionError. Otherwise returns zero
+    than zero, to avoid ZeroDivisionError. Otherwise returns zero.
     :rtype: float, 0
-    '''
+    """
+
     if distance > 0:
         return round(pheromone * ((1 / distance) ** beta), 4)
     else:
         return 0
 
 
-def get_reducted_value_of_path(element_value, list):
-    '''Gets the path value relative to the sum of all elements of the list
+def get_reducted_value_of_path(element_value, array):
+    """Gets the path value relative to the sum of all elements of the list
 
     :param element_value: Value of the element from the list
     :type element_value: float, 0
-    :param list: List containing all the values
-    :type list: list
+    :param array: List containing all the values
+    :type array: list
     :return: Returns reducted value of the element
     :rtype: float
-    '''
-    return element_value / sum(list)
+    """
+
+    return element_value / sum(array)
 
 
-def sum_matrix(first_matrix, second_matrix):
+def summation_of_matrices(first_matrix, second_matrix):
+    """Adding two double dimensional lists. Uses to update pheromone matrix and save survived ant path to
+    increase chance of choosing the way of the ant that created second matrix
+
+    :param first_matrix: base pheromone matrix that ants use
+    :type first_matrix: list
+    :param second_matrix: pheromone matrix that represents ant's path
+    :type second_matrix: list
+    :return: new global pheromone matrix that ants will be using
+    :rtype: list
+    """
+
     result_matrix = [[0 for _ in range(len(first_matrix))]
                      for _ in range(len(first_matrix))]
+
     for i in range(len(first_matrix)):
         for j in range(len(second_matrix)):
             result_matrix[i][j] = first_matrix[i][j] + second_matrix[i][j]
+
     return result_matrix
 
 
-def sub_matrix(first_matrix, second_matrix):
+def subtraction_of_matrices(first_matrix, second_matrix):
+    """Subtracting one matrix from another. Uses to update pheromone matrix and save died ant path to
+    decrease chance of choosing the way of the ant that created second matrix
+
+    :param first_matrix: base pheromone matrix that ants use
+    :type first_matrix: list
+    :param second_matrix: pheromone matrix that represents ant's path
+    :type second_matrix: list
+    :return: new global pheromone matrix that ants will be using
+    :rtype: list
+    """
+
     result_matrix = [[0 for _ in range(len(first_matrix))]
                      for _ in range(len(first_matrix))]
+
     for first_element in range(len(first_matrix)):
         for second_element in range(len(first_matrix)):
             temp = round(first_matrix[first_element][second_element] -
@@ -59,31 +92,78 @@ def sub_matrix(first_matrix, second_matrix):
                 result_matrix[first_element][second_element] = 0.01
             else:
                 result_matrix[first_element][second_element] = temp
+
     return result_matrix
 
 
-def choose_way_random(array):
-    rand = round(random(), 4)
-    summ = 0
-    for element in range(len(array)):
-        if summ + array[element] > rand:
-            return element
-        else:
-            summ += array[element]
-    return len(array) - 1
+def get_random_way_from_list(array):
+    """Gets one way from the list of reducted values of the ways.
+
+    Comparing the random value to the sum of the raw values, starting from first until
+    the sum is not bigger than the random value from the random function. If sum of all
+    the values is not bigger than random value(due to rounding it's possible to reduce all
+    the values of the array to one, but still get the sum lower than one), than it will
+    get another random value and will try to get sum bigger than new random value.
+    Does this until sum of the values is not greater than random value.
+
+    :param array: list of reducted values
+    :type array: list
+    :return: identifier of element that represents it's number in the list
+    :rtype: int
+    """
+
+    while True:
+        rand = round(random(), 4)
+        summ = 0
+        for element in range(len(array)):
+            if summ + array[element] > rand:
+                return element
+            else:
+                summ += array[element]
 
 
-def choose_way(temp_distance_matrix, pheromone_matrix, current_city):
-    raw_values_array = list(map(
-        get_raw_value_of_path, temp_distance_matrix[current_city],
-        pheromone_matrix[current_city]))
-    reducted_value_array =\
-        [round(get_reducted_value_of_path(value, raw_values_array), 4)
-         for value in raw_values_array]
-    return choose_way_random(reducted_value_array)
+def choose_from_suitable_ways(temp_distance_matrix, pheromone_matrix, current_city, beta):
+    # TODO:
+    #     doc
+    """
+
+    :param temp_distance_matrix:
+    :param pheromone_matrix:
+    :param current_city:
+    :param beta: Coefficient of how much ants relies on pheromone value
+    :type beta: float
+    :return:
+    :rtype:
+    """
+
+    raw_values_array = \
+        list(
+            map(
+                get_raw_value_of_path, temp_distance_matrix[current_city], pheromone_matrix[current_city], repeat(beta))
+        )
+    reducted_value_array = \
+        [round(
+            get_reducted_value_of_path(value, raw_values_array), 4) for value in raw_values_array]
+
+    return get_random_way_from_list(reducted_value_array)
 
 
-def one_try(distance_matrix, pheromone_matrix, amount_of_ants):
+def one_try(distance_matrix, pheromone_matrix, amount_of_ants, beta):
+    # TODO:
+    #   doc
+    #   add check for TabuList
+    #   add blocking of all the ways that goes to a visited vertices
+    """
+
+    :param distance_matrix:
+    :param pheromone_matrix:
+    :param amount_of_ants:
+    :param beta: Coefficient of how much ants relies on pheromone value, gets passed to subfunction
+    :type beta: float
+    :return:
+    :rtype:
+    """
+
     # set variables for ability to compare later
     max_distance = 0
     total_distance = 0
@@ -96,11 +176,10 @@ def one_try(distance_matrix, pheromone_matrix, amount_of_ants):
         current_city = 0
         city_to_visit = deepcopy(distance_matrix[0])
         temp_distance_matrix = deepcopy(distance_matrix)
-        temp_pheromone_matrix = [[0 for i in range(len(pheromone_matrix))]
-                                 for i in range(len(pheromone_matrix))]
+        temp_pheromone_matrix = [[0 for _ in range(len(pheromone_matrix))]
+                                 for _ in range(len(pheromone_matrix))]
 
         while True:
-
             # if not all cities are visited
             if not all(city == -1 for city in city_to_visit):
 
@@ -109,11 +188,12 @@ def one_try(distance_matrix, pheromone_matrix, amount_of_ants):
                            temp_distance_matrix[current_city]):
 
                     # using method to choose the way out of all available
-                    # pathes from current point
-                    chosen_way = choose_way(
+                    # paths from current point
+                    chosen_way = choose_from_suitable_ways(
                         temp_distance_matrix,
                         pheromone_matrix,
-                        current_city)
+                        current_city,
+                        beta)
 
                     # updating temporal variables
                     # to track current state
@@ -130,7 +210,7 @@ def one_try(distance_matrix, pheromone_matrix, amount_of_ants):
                 else:
                     # decrease value of pheromones on path because
                     # it leads to death
-                    pheromone_matrix = sub_matrix(
+                    pheromone_matrix = subtraction_of_matrices(
                         pheromone_matrix, temp_pheromone_matrix)
                     break
             else:
@@ -138,29 +218,35 @@ def one_try(distance_matrix, pheromone_matrix, amount_of_ants):
                 # update global variables to collect statistic
                 ants_survived += 1
                 total_distance += temp_total_distance
-                pheromone_matrix = sum_matrix(
+                pheromone_matrix = summation_of_matrices(
                     pheromone_matrix, temp_pheromone_matrix)
                 if max_distance < temp_total_distance:
                     max_distance = temp_total_distance
                 if min_distance > temp_total_distance > 0:
                     min_distance = temp_total_distance
                 break
+    print(min_distance)
 
 
 def main():
+    # TODO:
+    #     doc
+    #     apply threads:
+    #         apply gradient descent
     with open('distance_matrix.data', 'rb') as file:
         o_distance_matrix = load(file)
 
     default_pheromone_value = 0.01
     pheromone_matrix = [[default_pheromone_value
-                        for _ in range(len(o_distance_matrix))]
+                         for _ in range(len(o_distance_matrix))]
                         for _ in range(len(o_distance_matrix))]
     alpha = 0.1
     beta = 0.5
+    one_try(o_distance_matrix, pheromone_matrix, 200, beta)
 
 
 if __name__ == "__main__":
     start_time = time()
     main()
-    # display excecution time
-    print(f'Total excecution time: {round(time() - start_time, 3)}s\n\n')
+    # display execution time
+    print(f'Total execution time: {round(time() - start_time, 3)}s\n\n')
--- get_distance_matrix.py
+++ get_distance_matrix.py
@@ -1,13 +1,13 @@
-from settings import API_KEY
-import requests
 import pickle
 import time
 
+import requests
+
+from settings import API_KEY
 
 distance_matrix = []
 city_array = []
 counter = 0
-
 
 with open('cities.txt', 'r') as file:
     for line in file:
@@ -18,13 +18,13 @@ def get_place_id(place_str):
     while True:
         try:
             result = requests.get('https://maps.googleapis.com/maps/api/'
-            + 'place/findplacefromtext/json?key='
-            + API_KEY + '&input=' + place_str + '&inputtype=textquery').json()
-            return(result.get('candidates')[0].get('place_id'))
+                                  + 'place/findplacefromtext/json?key='
+                                  + API_KEY + '&input=' + place_str + '&inputtype=textquery').json()
+            return result.get('candidates')[0].get('place_id')
         except:
             print(result)
             time.sleep(60)
-            
+
 
 def get_distance_between_cities(first_city, second_city):
     fc_place_id = get_place_id(first_city)
@@ -33,7 +33,7 @@ def get_distance_between_cities(first_city, second_city):
     destination = sc_place_id
 
     r = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=place_id:'
-    + origin + '&destinations=place_id:' + destination + '&key=' + API_KEY).json()
+                     + origin + '&destinations=place_id:' + destination + '&key=' + API_KEY).json()
 
     km = r.get('rows')[0].get('elements')[0].get('distance').get('value')
     distance = km // 1000
@@ -50,11 +50,10 @@ for i in city_array:
             dist = -1
         counter += 1
         print('Distance between ' + i + ' and ' + j + ' is ' + str(dist) + 'km')
-        print(str(counter/25) + '% done')
+        print(str(counter / 25) + '% done')
         print(f'Request took {round(execution_time, 2)} seconds\n')
         distance_matrix[-1].append(dist)
         time.sleep(5)
-
 
 with open('distance_matrix.data', 'wb') as f:
     pickle.dump(distance_matrix, f)
